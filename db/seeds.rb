250e8aba9f69e1854368dc500ac2115602f3df6d
Alexander Sinelnik <ssinelnik@bk.ru>
2024-07-01 12:25:16 +0500

    Format seeds _id, .id

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,61 +28,61 @@ categories = Category.create!([
 ])
 
 tests =  Test.create!([
-  { title: "Ruby", level: 1, category_id: categories[1].id, author_id: users[0].id },
-  { title: "Rails", level: 2, category_id: categories[1].id, author_id: users[0].id },
-  { title: "HTML", level: 1, category_id: categories[0].id, author_id: users[1].id },
-  { title: "CSS", level: 1, category_id: categories[0].id, author_id: users[1].id },
-  { title: "JavaScript", level: 2, category_id: categories[0].id, author_id: users[1].id },
-  { title: "R", level: 2, category_id: categories[2].id, author_id: users[3].id },
-  { title: "C/C++", level: 3, category_id: categories[2].id, author_id: users[2].id },
-  { title: "Python", level: 2, category_id: categories[3].id, author_id: users[2].id },
-  { title: "Java", level: 2, category_id: categories[4].id, author_id: users[4].id },
-  { title: "Kotlin", level: 1, category_id: categories[4].id, author_id: users[4].id },
-  { title: "Fortran", level: 3, category_id: categories[5].id, author_id: users[3].id },
-  { title: "Matlab", level: 3, category_id: categories[5].id, author_id: users[3].id }
+  { title: "Ruby", level: 1, category: categories[1], author: users[0] },
+  { title: "Rails", level: 2, category: categories[1], author: users[0]},
+  { title: "HTML", level: 1, category: categories[0], author: users[1] },
+  { title: "CSS", level: 1, category: categories[0], author: users[1] },
+  { title: "JavaScript", level: 2, category: categories[0], author: users[1] },
+  { title: "R", level: 2, category: categories[2], author: users[3] },
+  { title: "C/C++", level: 3, category: categories[2], author: users[2] },
+  { title: "Python", level: 2, category: categories[3], author: users[2] },
+  { title: "Java", level: 2, category: categories[4], author: users[4] },
+  { title: "Kotlin", level: 1, category: categories[4], author: users[4] },
+  { title: "Fortran", level: 3, category: categories[5], author: users[3] },
+  { title: "Matlab", level: 3, category: categories[5], author: users[3] }
 ])
 
 questions = Question.create!([
-  { body: "What is the name of a specialized data type in the Ruby language?", test_id: tests[0].id },
-  { body: "What does ORM stand for?", test_id: tests[1].id },
-  { body: "What encoding should use on the site?", test_id: tests[2].id },
-  { body: "What does CSS stand for?", test_id: tests[3].id },
-  { body: "Where is the correct place to insert a JavaScript?", test_id: tests[4].id },
-  { body: "In R, you must declare the data type of a variable?", test_id: tests[5].id },
-  { body: "C++ is an alias of C#?", test_id: tests[6].id },
-  { body: "Which collection does not allow duplicate members?", test_id: tests[7].id },
-  { body: "Java is short for JavaScript?", test_id: tests[8].id },
-  { body: "Which keyword is used to declare a function?", test_id: tests[9].id },
-  { body: "Is Fortran interpreter type language?", test_id: tests[10].id },
-  { body: "Which commands are used to display the content of the workspace?", test_id: tests[11].id }
+  { body: "What is the name of a specialized data type in the Ruby language?", test: tests[0] },
+  { body: "What does ORM stand for?", test: tests[1] },
+  { body: "What encoding should use on the site?", test: tests[2] },
+  { body: "What does CSS stand for?", test: tests[3] },
+  { body: "Where is the correct place to insert a JavaScript?", test: tests[4] },
+  { body: "In R, you must declare the data type of a variable?", test: tests[5] },
+  { body: "C++ is an alias of C#?", test: tests[6] },
+  { body: "Which collection does not allow duplicate members?", test: tests[7] },
+  { body: "Java is short for JavaScript?", test: tests[8] },
+  { body: "Which keyword is used to declare a function?", test: tests[9] },
+  { body: "Is Fortran interpreter type language?", test: tests[10] },
+  { body: "Which commands are used to display the content of the workspace?", test: tests[11] }
 ])
 
 answers = Answer.create!([
-  { body: "Symbols", question_id: questions[0].id },
-  { body: "Object Relational Mapper", question_id: questions[1].id },
-  { body: "UTF-8", question_id: questions[2].id },
-  { body: "Cascading Style Sheets", question_id: questions[3].id },
-  { body: "Body", question_id: questions[4].id },
-  { body: "No", question_id: questions[5].id },
-  { body: "No", question_id: questions[6].id },
-  { body: "Set", question_id: questions[7].id },
-  { body: "No", question_id: questions[8].id },
-  { body: "Fun", question_id: questions[9].id },
-  { body: "Yes", question_id: questions[10].id },
-  { body: "Who", question_id: questions[11].id }
+  { body: "Symbols", question: questions[0] },
+  { body: "Object Relational Mapper", question: questions[1] },
+  { body: "UTF-8", question: questions[2] },
+  { body: "Cascading Style Sheets", question: questions[3] },
+  { body: "Body", question: questions[4] },
+  { body: "No", question: questions[5] },
+  { body: "No", question: questions[6] },
+  { body: "Set", question: questions[7] },
+  { body: "No", question: questions[8] },
+  { body: "Fun", question: questions[9] },
+  { body: "Yes", question: questions[10] },
+  { body: "Who", question: questions[11] }
 ])
 
 tests_user = TestsUser.create!([
-  { test_id: tests[0].id, user_id: users[0].id },
-  { test_id: tests[1].id, user_id: users[0].id },
-  { test_id: tests[2].id, user_id: users[1].id },
-  { test_id: tests[3].id, user_id: users[1].id },
-  { test_id: tests[4].id, user_id: users[1].id },
-  { test_id: tests[5].id, user_id: users[3].id },
-  { test_id: tests[6].id, user_id: users[2].id },
-  { test_id: tests[7].id, user_id: users[2].id },
-  { test_id: tests[8].id, user_id: users[4].id },
-  { test_id: tests[9].id, user_id: users[4].id },
-  { test_id: tests[10].id, user_id: users[3].id },
-  { test_id: tests[11].id, user_id: users[3].id }
+  { test: tests[0], user: users[0] },
+  { test: tests[1], user: users[0] },
+  { test: tests[2], user: users[1] },
+  { test: tests[3], user: users[1] },
+  { test: tests[4], user: users[1] },
+  { test: tests[5], user: users[3] },
+  { test: tests[6], user: users[2] },
+  { test: tests[7], user: users[2] },
+  { test: tests[8], user: users[4] },
+  { test: tests[9], user: users[4] },
+  { test: tests[10], user: users[3] },
+  { test: tests[11], user: users[3] }
 ])
