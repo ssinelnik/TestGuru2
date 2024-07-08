cb044716e7932e6b51900abf03f606736c914161
Alexander Sinelnik <ssinelnik@bk.ru>
2024-07-08 17:56:06 +0500

    Change scope-method name to :correct

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   validate :validate_max_answers, on: :create
 
-  scope :correct_answer, -> { where(correct: true) }
+  scope :correct, -> { where(correct: true) }
 
   private
 
