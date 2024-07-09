e2097d9f0d77cb8e3da33808b89e81ee0f2a0aab
Alexander Sinelnik <ssinelnik@bk.ru>
2024-07-09 15:41:39 +0500

    Delete uniqueness for :title attribute in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   default_scope { order(title: :asc) }
   has_many :tests, dependent: :destroy
 
-  validate :title, presence: true, uniqueness: true
+  validate :title, presence: true
 end
