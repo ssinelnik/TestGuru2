54005cacbbe35d0df1a925f367f7388d0dc5b324
Alexander Sinelnik <ssinelnik@bk.ru>
2024-08-03 10:32:54 +0500

    delete extra

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  get "up" => "rails/health#show", as: :rails_health_check
-
   resources :tests do
     resources :questions, shallow: true
   end
