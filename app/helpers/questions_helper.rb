7af7f9b179a985ef9cdf2d8d0bf57b1a370710de
AlexanderSinelnik <ssinelnik@bk.ru>
2024-10-07 11:15:20 +0500

    last changes

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,10 +2,13 @@
 
 module QuestionsHelper
 	def question_header(question)
-		if question.new_record?
-			'Create new question'
-		else
-			'Edit question'
-		end
+		# if question.new_record?
+		# 	'Create new question'
+		# else
+		# 	'Edit question'
+		# end
+
+		intro = question.new_record? ? 'Create New' : 'Edit'
+    "#{intro} #{question.test.title} question"
 	end
 end
