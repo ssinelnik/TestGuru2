68b9266965bddadfbad58c8d3e2f390fe0181bff
AlexanderSinelnik <ssinelnik@bk.ru>
2024-10-19 01:15:23 +0500

    add admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_18_150206) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_18_194504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,9 +74,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_18_150206) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
   add_foreign_key "answers", "questions"
