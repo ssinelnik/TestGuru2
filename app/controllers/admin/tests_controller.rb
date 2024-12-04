0b2eea14d4448021411df882b788e3ea0b5292a4
AlexanderSinelnik <ssinelnik@bk.ru>
2024-12-04 19:02:43 +0500

    data attributes and progress bar

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::TestsController < Admin::BaseController
-  before_action :set_test, only: %i[show edit update destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[show edit update destroy update_inline]
 
   def index
     @tests = Test.all
@@ -36,12 +37,24 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline 
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
   end
 
   private
+
+  def set_tests
+    @test = Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
