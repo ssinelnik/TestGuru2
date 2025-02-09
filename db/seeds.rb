7aaf59774eac13c5ef2cca0967f235ea8d438441
AlexanderSinelnik <ssinelnik@bk.ru>
2025-02-09 08:19:52 +0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,8 @@
 return if User.exists? && Test.exists? && Category.exists?
 
 users = User.create!([
-  { first_name: "Alexander", email: "alexander1999@mail.com" },
-  { first_name: "Anna", email: "anna2003@mail.com" },
-  { first_name: "Georgy", email: "georgy2001@mail.com" },
-  { first_name: "Artem", email: "artem2005@mail.com" },
-  { first_name: "Ekaterina", email: "ekaterina2000@mail.com" }
+  { first_name: "Alexander", last_name: "Sinelnik", email: "ssinelnik@bk.ru", password: "123456", type: "Admin" },
+  { first_name: "Steve", last_name: "Jobs", email: "abcnicenme@gmail.com", password: "123456", type: "Admin" }
   ])
 
 categories = Category.create!([
@@ -78,11 +75,11 @@ tests_user = TestsUser.create!([
   { test: tests[2], user: users[1] },
   { test: tests[3], user: users[1] },
   { test: tests[4], user: users[1] },
-  { test: tests[5], user: users[3] },
-  { test: tests[6], user: users[2] },
-  { test: tests[7], user: users[2] },
-  { test: tests[8], user: users[4] },
-  { test: tests[9], user: users[4] },
-  { test: tests[10], user: users[3] },
-  { test: tests[11], user: users[3] }
+  { test: tests[5], user: users[0] },
+  { test: tests[6], user: users[0] },
+  { test: tests[7], user: users[0] },
+  { test: tests[8], user: users[1] },
+  { test: tests[9], user: users[1] },
+  { test: tests[10], user: users[1] },
+  { test: tests[11], user: users[1] }
 ])
