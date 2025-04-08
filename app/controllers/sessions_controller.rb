b5a6401323641af47473f04e255e5b8dde28c15a
AlexanderSinelnik <ssinelnik@bk.ru>
2025-04-08 13:30:06 +0500

    fix logout and sorting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,4 @@ class SessionsController < Devise::SessionsController
     super
     flash[:notice] = t('shared.message.hello', user_first_name: current_user.first_name)
   end
-
-  #  def destroy
-  #   # Logic to log out the user (e.g., clearing the session)
-  #   session.delete(:id)
-  #   redirect_to root_path, notice: 'Logged out successfully.'
-  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 return if User.exists? && Test.exists? && Category.exists?
-
-# users = User.create!([
-#                       { first_name: 'Steve', last_name: 'Jobs',
-#                         email: 'abcnicenme@gmail.com', password: '123456', type: 'Admin',
-#                         confirmation_token: 'pUzjpCCFDVSuc21xU23s', confirmed_at: '2025-01-16 13:30:09.429851', confirmation_sent_at: '2025-01-16 13:30:04.889953' }
-#                     ])
-
-# categories = Category.create!([
-#   { title: "Backend" }
-# ])
-
-# tests =  Test.create!([
-#   { title: "Ruby", level: 1, category: categories[1], author: users[0] },
-#   { title: "Rails", level: 2, category: categories[1], author: users[0]}
-# ])
-
-# questions = Question.create!([
-#   { body: "What is the name of a specialized data type in the Ruby language?", test: tests[0] },
-#   { body: "What does ORM stand for?", test: tests[1] }
-# ])
-
-# answers = Answer.create!([
-#   { body: "Symbols", question: questions[0] },
-#   { body: "Object Relational Mapper", question: questions[1] }
-# ])
 
 users = User.create!([
                       { first_name: 'Alexander', last_name: 'Sinelnik',
                         email: 'sinelnik@mail.com', password: '123456', type: 'Admin',
-                        confirmation_token: 'pUzjpCCFDVSuc21xU23s', confirmed_at: '2025-01-16 13:30:09.429851', confirmation_sent_at: '2025-01-16 13:30:04.889953' }
+                        confirmation_token: 'pUzjpCCFDVSuc21xU23s', confirmed_at: '2025-01-16 13:30:09.429851', confirmation_sent_at: '2025-01-16 13:30:04.889953' },
+                      { first_name: 'Steve', last_name: 'Jobs',
+                        email: 'stevejobs@mail.ru', password: '123456', type: 'User',
+                        confirmation_token: 'YoXDyxQdnb1zrYwr78u2', confirmed_at: '2025-04-07 21:54:38.13375', confirmation_sent_at: '2025-04-07 21:54:25.500785' }
                     ])
 
 categories = Category.create!([
