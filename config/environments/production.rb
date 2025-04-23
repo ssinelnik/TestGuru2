19aaaec7bc8df5e80becb96e95876d5130778edc
AlexanderSinelnik <ssinelnik@bk.ru>
2025-04-24 00:40:00 +0500

    change 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = true
   config.action_mailer.default_url_options = { host: 'testguru2.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
