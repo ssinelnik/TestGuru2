77248e856d060b905ec2bc2da49e995e267bd294
AlexanderSinelnik <ssinelnik@bk.ru>
2025-04-30 02:12:10 +0500

    rubocop feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class Feedback < ApplicationRecord
-  validates :user_name, :email, :header, :content, presence: true
+  validates :user_name,
+            :email,
+            :header,
+            :content,
+            presence: true
 end
